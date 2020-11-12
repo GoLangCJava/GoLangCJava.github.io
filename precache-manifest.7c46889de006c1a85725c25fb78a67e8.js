00ff3c2f706059c66d63a56a3c398f2cde97d791
wade.chen@nike.com <wade.chen@nike.com>
2020-11-12 10:40:14 +0800

    i

---
+++ precache-manifest.7c46889de006c1a85725c25fb78a67e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d09bd25d6035c8522ee74effff62fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "637bc5ebc98f5ba7648f",
+    "url": "/static/css/main.8e9ffea4.chunk.css"
+  },
+  {
+    "revision": "575db461e1f829b52847",
+    "url": "/static/js/2.274caa97.chunk.js"
+  },
+  {
+    "revision": "637bc5ebc98f5ba7648f",
+    "url": "/static/js/main.677d948d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
